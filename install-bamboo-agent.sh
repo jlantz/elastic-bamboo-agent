3bbe1a8ce0ad95e9574a6016757b1d2709e2daca
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-06-28 21:30:54 -0400

    install script updates

--- install-bamboo-agent.sh
+++ install-bamboo-agent.sh
@@ -3,18 +3,26 @@
 
 # Timeout for the agent start.
 # Increase the timeout if the script does not get to `Bamboo agent 'Elastic Agent on <ec2-instance-id>' ready to receive builds.`.
-AGENT_TIMEOUT="5m"
+AGENT_TIMEOUT="3m"
 
-echo "    [Bamboo Agent installation script]: Installing Java and other packages.."
+# Enable the multiverse repos
+sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install openjdk-7-jdk -y
+
+echo -e "\n\n[Bamboo Agent installation script]: Installing AWS Tools...\n\n"
+#sudo apt-add-repository ppa:awstools-dev/awstools -y
+sudo apt-get install -y awscli ec2-api-tools
+
+echo -e "\n\n[Bamboo Agent installation script]: Installing Java...\n\n"
+sudo apt-get install -y openjdk-7-jdk
+echo -e "\nexport JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" | sudo tee -a /etc/environment
 
 java -version
 
-echo "    [Bamboo Agent installation script]: Installing other packages..."
-sudo apt-get install unzip htop -y
+echo -e "\n\n\[Bamboo Agent installation script]: Installing other packages...\n\n"
+sudo apt-get install -y mc unzip htop
 
-echo "    [Bamboo Agent installation script]: Installing Docker, Docker Compose..."
+echo -e "\n\n[Bamboo Agent installation script]: Installing Docker, Docker Compose...\n\n"
 wget -qO- https://get.docker.com/ | sh
 curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` | sudo dd of=/usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -22,13 +30,13 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo docker version
 sudo docker-compose --version
 
-echo "    [Bamboo Agent installation script]: Configuring users.."
+echo -e "\n\n[Bamboo Agent installation script]: Configuring users..\n\n"
 sudo useradd -m bamboo --shell /bin/bash
 sudo usermod -aG docker ubuntu
 sudo usermod -aG docker bamboo
 
-echo "    [Bamboo Agent installation script]: Installing Bamboo Agent..."
-export imageVer=4.1
+echo -e "\n\n[Bamboo Agent installation script]: Installing Bamboo Agent...\n\n"
+export imageVer=4.2
 wget https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/bamboo/atlassian-bamboo-elastic-image/${imageVer}/atlassian-bamboo-elastic-image-${imageVer}.zip
 sudo mkdir -p /opt/bamboo-elastic-agent
 sudo unzip -o atlassian-bamboo-elastic-image-${imageVer}.zip -d /opt/bamboo-elastic-agent
@@ -36,20 +44,22 @@ sudo chown -R bamboo /opt/bamboo-elastic-agent
 sudo chmod u+r+w /opt/bamboo-elastic-agent
 sudo chmod 755 /opt/bamboo-elastic-agent/*
 
-echo "    [Bamboo Agent installation script]: Configuring paths..."
-echo -e "\nexport JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" | sudo tee -a /etc/profile.d/bamboo.sh
-echo -e "\nexport PATH=/opt/bamboo-elastic-agent/bin:$JAVA_HOME/bin:$PATH"  | sudo tee -a /etc/profile.d/bamboo.sh
+echo -e "\n\n[Bamboo Agent installation script]: Configuring paths...\n\n"
+echo -e "\nexport PATH=$PATH:$JAVA_HOME/bin:/opt/bamboo-elastic-agent/bin"  | sudo tee -a /etc/profile.d/bamboo.sh
 
-echo "    [Bamboo Agent installation script]: Configuring Bamboo Agent autostart"
-sudo sed -i 's/exit 0/#exit 0/' /etc/rc.local
-echo -e "\n#Configure automatic startup of the Bamboo agent\n. /opt/bamboo-elastic-agent/etc/rc.local\n" | sudo tee -a /etc/rc.local
+echo -e "\n\n[Bamboo Agent installation script]: Configuring Bamboo Agent autostart\n\n"
+# Configure automatic startup of the Bamboo agent (add before line 14 of /etc/rc.local
+sudo sed -i '14 i . /opt/bamboo-elastic-agent/etc/rc.local\n' /etc/rc.local
+#sudo sed -i 's/exit 0/#exit 0/' /etc/rc.local
+#echo -e "\n#Configure automatic startup of the Bamboo agent\n. /opt/bamboo-elastic-agent/etc/rc.local\n" | sudo tee -a /etc/rc.local
 
-echo "    [Bamboo Agent installation script]: Starting Bamboo Agent..."
+echo -e "\n\n[Bamboo Agent installation script]: Starting Bamboo Agent...\n\n"
 sudo su -c "timeout $AGENT_TIMEOUT /opt/bamboo-elastic-agent/bin/bamboo-elastic-agent" - bamboo
 
-echo "    [Bamboo Agent installation script]: Finalizing ..."
+echo -e "\n\n[Bamboo Agent installation script]: Finalizing ...\n\n"
+# Welcome screen
 sudo cp /opt/bamboo-elastic-agent/etc/motd /etc/motd
-echo bamboo-${imageVer}  | sudo tee -a /etc/motd
+echo bamboo-agent-${imageVer}  | sudo tee -a /etc/motd
 sudo rm -f /root/firstlogin /etc/ssh/ssh_host_dsa_key /etc/ssh/ssh_host_dsa_key.pub /etc/ssh/ssh_host_key /etc/ssh/ssh_host_key.pub /etc/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key.pub /root/.ssh/authorized_keys
 sudo touch /root/firstrun
 sudo /opt/bamboo-elastic-agent/bin/prepareInstanceForSaving.sh
